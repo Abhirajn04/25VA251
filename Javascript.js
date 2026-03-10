f53a09c9420ac73cfc51ced53cbac8e13feb7fd2
Abhiraj Niranjan <abhiraj.25b01011115@abes.ac.in>
2026-03-10 09:49:57 +0530

    javascript

---
+++ Javascript.js
@@ -0,0 +1,13 @@
+//1. Javascript is a cross platform object based scripting language for client and server application., It is used everywhere.
+//2. Loosely typed language.
+//3. It is an interpreted language.
+//4. It is a scripting language.
+//5. It is an object based language.
+//6. It is a case sensitive language.
+//7. It is a dynamic language.
+//8. It is a prototype based language.
+//9. It is a multi paradigm language.
+//10. It is a single threaded language.
+//11. It is a functional programming language.
+//12. It is a event driven language.
+
